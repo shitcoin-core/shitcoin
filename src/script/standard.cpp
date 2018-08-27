0c1da4f7e6a8a917700055916f14d74fbf16ea88
onokatio <onokatio@gmail.com>
2018-08-27 10:55:51 +0900

    fix line removeg missiing

--- src/script/standard.cpp
+++ src/script/standard.cpp
@@ -95,7 +95,6 @@ bool Solver(const CScript& scriptPubKey, txnouttype& typeRet, std::vector<std::v
     }
 
     // Scan templates
-    const CScript& script1 = scriptPubKey;
     for (const std::pair<txnouttype, CScript>& tplate : mTemplates)
     {
         const CScript& script2 = tplate.second;
